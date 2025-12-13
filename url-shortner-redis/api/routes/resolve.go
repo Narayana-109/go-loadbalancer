bbaf9e6e9d54392d9f07b609beff483efa4e02f2
narayana-109 <narayanapv777@gmail.com>
2025-12-14 01:31:40 +0530

    Added helpers, new db client, resolve url funcs.

---
+++ url-shortner-redis/api/routes/resolve.go
@@ -0,0 +1,34 @@
+package routes
+
+import (
+	"github.com/Narayana-109/go-projects/tree/main/url-shortner-redis/database"
+	redis "github.com/go-redis/redis/v8"
+	"github.com/gofiber/fiber/v2"
+)
+
+
+func ResolveURL(c *fiber.Ctx) error {
+
+	url := c.Params("url")
+
+	r := database.CreateClient(0)
+	defer r.Close()
+
+	value, err := r.Get(database.Ctx, url).Result()
+	if err != redis.Nil{
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map(
+			"error":"short url not found in the database"
+			))
+	} else if err != nil {
+		return c.Status(fiber.StatusInternalError).JSON(fiber.Map{
+			"error":"cannot connect to DB"
+		})
+	}
+
+	rInr := databse.CreateClient(1)
+	defer rInr.close()
+
+	_ = rInr.Incr(databse.Ctx, "counter")
+	return c.Redirect(value, 301)
+
+}
